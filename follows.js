7aadb94d3a159199bb0c7cebb559548caed4eb4b
spatel3220 <shivang.patel@gmail.com>
2014-11-29 00:04:53 -0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,7 +12,7 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
+	var defaultKey		= '9M8kqb3wyO4BCWJ2dexfVjFlHKbCZEXb0tTQtFm52AB6UlAd', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1717676570], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Jarvis - indoor monitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
